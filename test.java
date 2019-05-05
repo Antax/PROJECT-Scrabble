42e311ce4bf145d36412682ec10910af5d6daa9f
Antax <50098795+Antax@users.noreply.github.com>
2019-05-05 16:45:57 +0200

    Plateau et Case
    Instanciation du plateau et méthode toString du plateau : fonctionnel
    Fonction pour placer une lettre dans une case du tableau : non fonctionnel
    Instanciations d'une case (avec ou sans lettre, avec ou sans type) et méthode toString d'une case : fonctionnel

--- test.java
+++ test.java
@@ -1,12 +1,25 @@
 public class test{
    public static void main(String[] args) {
-        SacDeLettres sac = new SacDeLettres();
+        /*SacDeLettres sac = new SacDeLettres();
         String strSac=sac.SacToString();
         System.out.println(strSac);
         Chevalet c =new Chevalet ();
         c.remplirChevalet(sac);
         String strC=c.chevaletToString();
         System.out.println(strC);
-        System.out.println(strSac);
+        System.out.println(strSac);*/
+
+        /*CaseDePlateau caseCl = new CaseDePlateau("m2");
+        String strCase = caseCl.caseToString();
+        System.out.println(strCase);
+        caseCl.lettre=new Lettre('A');
+        strCase = caseCl.caseToString();
+        System.out.println(strCase);*/
+        Lettre A = new Lettre ('A');
+        Plateau plateau = new Plateau();
+        String strPlateau = plateau.plateauToString();
+        System.out.println(strPlateau);
+        plateau.placerLettre(A, 14, 14);
+        System.out.println(strPlateau);
    } 
 }
